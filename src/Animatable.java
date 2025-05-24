82e81e3f17e545bfac2f8024274c7cc521b8ee7c
Stanley-Lam <stanley.l4974@gmail.com>
2025-05-24 15:16:11 -0700

    initial code3

--- src/Animatable.java
+++
@@ -1,9 +0,0 @@
-package src;
-
-public interface Animatable {
-    double getAnimationPeriod();
-
-    default void scheduleActions(EventScheduler scheduler, WorldModel world, ImageStore imageStore) {
-        scheduler.scheduleEvent((Entity) this, new Animation((Entity) this, world, imageStore, 0), getAnimationPeriod());
-    }
-}
