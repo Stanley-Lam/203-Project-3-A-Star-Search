e0a47226a6c8e2f4d5ba5dd508090af59195ce7c
Joshua-Win <jkwinn@calpoly.edu>
2025-05-29 17:52:38 -0700

    Joshua Winn - Changed Movable, AStarPathingStrategy, Dude, and Fairy

--- Movable.java
+++ Movable.java
@@ -1,14 +1,36 @@
 import processing.core.PImage;
 
 import java.util.List;
+import java.util.function.BiPredicate;
+import java.util.function.Predicate;
 
 public abstract class Movable extends ActiveAnimatedEntity {
+    protected static final PathingStrategy pathing_dude = new AStarPathingStrategy();
+    protected static final PathingStrategy pathing_fairy = new AStarPathingStrategy();
+    private PathingStrategy pathstrat;
 
-    public Movable(String id, Point position, List<PImage> images, double actionPeriod, double animationPeriod) {
+    public Movable(String id, Point position, List<PImage> images, double actionPeriod, double animationPeriod, PathingStrategy pathstrat) {
         super(id, position, images, actionPeriod, animationPeriod);
+        this.pathstrat = pathstrat;
     }
 
+
     public Point nextPosition(WorldModel world, Point destPos) {
+        Predicate<Point> canPassThrough = (Point p) -> world.withinBounds(p) && !world.isOccupied(p);
+        BiPredicate<Point, Point> withinReach = (Point p1, Point p2) -> p1.adjacent(p2);
+
+        List<Point> path = pathstrat.computePath(getPosition(), destPos, canPassThrough, withinReach,
+                PathingStrategy.CARDINAL_NEIGHBORS);
+
+        if (path.size() == 0){
+            return this.getPosition();
+        }
+        else{
+            return path.get(0);
+        }
+    }
+
+    /*public Point nextPosition(WorldModel world, Point destPos) {
         int horiz = Integer.signum(destPos.x - this.getPosition().x);
         Point newPos = new Point(this.getPosition().x + horiz, this.getPosition().y);
 
@@ -22,7 +44,8 @@ public abstract class Movable extends ActiveAnimatedEntity {
         }
 
         return newPos;
-    }
+    }*/
+
     public abstract boolean moveTo(WorldModel model, Entity target, EventScheduler scheduler);
 
     /**
